d210f301d5e66566ec6ae1b4dd635f21e7648af5
Tushar <tusharsingh.official@icloud.com>
2022-01-13 19:30:21 +0530

    home-page-done

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -18,7 +18,7 @@ const Sidedrawer = () => {
                 <li><Link to='/archive'>Archive</Link></li>
                 <li><Link to='/team'>Our Team</Link></li>
                 <li><Link to='/terms_and_condtions'>Terms and Condtions</Link></li>
-                <li><a href="#" className="btn btn_primary show_720px">Grab a Ticket</a></li>
+                <li><a href="https://razorpay.com/" className="btn btn_primary show_720px">Grab a Ticket</a></li>
             </ul>
             <button onClick={handleCloseNav} className='close_btn'>
                 <i className="fa fa-times"></i>
